b33959dcb95ad210dcd7d689aa7a84b0b36c5628
bolivar2k <GarciaEscobar@DESKTOP-E89E69U>
2021-11-28 12:13:08 +0100

    First uploading - template.

---
+++ SimpleSpringBatch/src/test/java/com/bolivar2k/batchprocessing/simple/SimpleSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolivar2k.batchprocessing.simple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
